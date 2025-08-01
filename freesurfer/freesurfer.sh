16769abb98b4ce70bb04c71149e8d83a843f9715
arovai <antonin.rovai@ulb.be>
2025-08-01 17:00:42 +0200

    update

---
+++ freesurfer/freesurfer.sh
@@ -0,0 +1,135 @@
+#!/bin/bash
+
+# Default variables
+DEFAULT_APPTAINER_DIR="${HOME}/apptainer"
+DEFAULT_VERSION="7.3.2"  # As of 20250801, version 7.3.2 is the once included in latest fmriprep (25.1.4)
+DEFAULT_FS_LICENSE="/opt/freesurfer/.license"
+
+# Apptainer variables
+APPTAINER_CMD="/usr/bin/apptainer"
+APPTAINER_OPT="--nv --cleanenv"
+
+# Display usage information
+usage() {
+    echo "Usage: $0 [--input input_dir] [--output output_dir] [--fs-license fs_license] [--apptainer-dir apptainer_dir] [--version version] [--participant-label participant_label] [--more more_options]"
+    exit 1
+}
+
+check_apptainer_is_installed() {
+  if [ ! -f "${APPTAINER_CMD}" ]; then
+    echo "Apptainer not found at ${APPTAINER_CMD}, are your sure it is installed?"
+    exit 1
+  fi
+}
+
+ensure_image_exists() {
+    local apptainer_dir="$1"
+    local version="$2"
+    local file_path="${apptainer_dir}/freesurfer.freesurfer.${version}.sif"
+    local create_command="${APPTAINER_CMD} build ${file_path} docker://freesurfer/freesurfer:${version}"
+
+    if [ ! -f "$file_path" ]; then
+        echo "File $file_path does not exist. Creating it..."
+        mkdir -p "${DEFAULT_APPTAINER_DIR}"
+        eval "$create_command"
+        if [ $? -eq 0 ]; then
+            echo "File $file_path created successfully."
+        else
+            echo "Failed to create file $file_path."
+            exit 1
+        fi
+    else
+        echo "File $file_path already exists."
+    fi
+
+    export APPTAINER_IMG="${file_path}"
+}
+
+check_file_exists() {
+    local file_path="$1"
+    if [ ! -f "$file_path" ]; then
+      echo "File $file_path does not exist. Abording..."
+      exit 1
+    else
+        echo "File $file_path found."
+    fi
+}
+
+# Initialize variables
+input_dir=""
+output_dir=""
+fs_license="${DEFAULT_FS_LICENSE}"
+apptainer_dir="${DEFAULT_APPTAINER_DIR}"
+version="${DEFAULT_VERSION}"
+more_options=""
+
+# Parse command line options
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        --input)
+            input_dir=$2
+            shift
+            ;;
+        --output)
+            output_dir=$2
+            shift
+            ;;
+        --fs-license)
+            fs_license=$2
+            shift
+            ;;
+        --apptainer-dir)
+            apptainer_dir=$2
+            shift
+            ;;
+        --version)
+            version=$2
+            shift
+            ;;
+        --participant-label)
+            participant_label=$2
+            shift
+            ;;
+        --more)
+            more_options=$2
+            shift
+            ;;
+        *)
+            echo "Unknown parameter passed: $1"
+            usage
+            ;;
+    esac
+    shift
+done
+
+# Check mandatory options
+if [ -z "$input_dir" ] || [ -z "$output_dir" ] || [ -z "$participant_label" ]; then
+    echo "Error: input, output and participant_label values must be specified."
+    usage
+fi
+
+# Main script logic
+echo "Input directory: $input_dir"
+echo "Output directory: $output_dir"
+echo "Apptainer directory: $apptainer_dir"
+echo "Freesurfer version: ${version:-Not specified}"
+echo "More options: ${more_options:-Not specified}"
+
+check_apptainer_is_installed
+ensure_image_exists "${apptainer_dir}" "${version}"
+
+participant_dir="${input_dir}/sub-${participant_label}"
+participant_T1w="${participant_dir}/anat/sub-${participant_label}_T1w.nii.gz"
+
+check_file_exists "${fs_license}"
+check_file_exists "${participant_T1w}"
+
+${APPTAINER_CMD} run ${APPTAINER_IMG} \
+  -B "${fs_license}":/usr/local/freesurfer/.license \
+  -B "${input_dir}":/rawdata \
+  -B "${output_dir}":/derivatives "${app}" \
+  recon-all -all \
+  -subjid sub-${participant_label} \
+  -i /rawdata/sub-${participant_label}/anat/sub-${participant_label}_T1w.nii.gz \
+  -T2 /rawdata/sub-${participant_label}/anat/sub-${participant_label}_FLAIR.nii.gz \
+  -sd /derivatives
