6b5a540e59d3e69361b7603561a2766094521555
arovai <antonin.rovai@ulb.be>
2025-08-06 15:31:02 +0200

    update

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # ln2t-tools
 Useful tools for the LN2T
+
+## Command-line Completion
+
+To enable command-line completion:
+
+1. Install the package:
+```bash
+pip install .
+```
+
+2. Source the completion script:
+```bash
+source /usr/local/share/bash-completion/completions/ln2t_tools_completion.bash
+```
+
+3. Add to your ~/.bashrc to make it permanent:
+```bash
+echo "source /usr/local/share/bash-completion/completions/ln2t_tools_completion.bash" >> ~/.bashrc
+```
--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ def read_requirements():
         return req.read().splitlines()
 
 setup(
-    name='ln2t_tools',
-    version='1.0.0',
+    name="ln2t_tools",
+    version="1.0.0",
     url='https://github.com/ln2t/ln2t_tools',
     author='Antonin Rovai',
     author_email='antonin.rovai@hubruxelles.be',
@@ -19,5 +19,8 @@ setup(
             'ln2t_tools = ln2t_tools.ln2t_tools:main',
         ]},
     include_package_data=True,
-    package_data={}
+    package_data={},
+    data_files=[
+        ('share/bash-completion/completions', ['ln2t_tools/completion/ln2t_tools_completion.bash']),
+    ],
 )
